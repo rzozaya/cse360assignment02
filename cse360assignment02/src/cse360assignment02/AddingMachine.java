6a937f224e0ff61b18dbb59ddb91b276db89ab8f
ramon <ramon@RaspberryPie>
2020-10-05 22:09:38 -0700

    Initial/Skeletal version of AddingMachine

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,40 @@
+package cse360assignment02;
+
+/******
+ * 	AddingMachine is a Class that can add and subtract
+ * to a 'total', initially set to 0.
+ * 
+ * @author  Ramon Zozaya
+ * @version 1.0  
+ * @since 	Oct 5, 2020
+ * 
+ */
+
+public class AddingMachine {
+	  private int total; 
+	  /****
+	   * This is a constructor that instantiates the class
+	   * AddingMachine and starts with an initial total of 0 
+	   * {@value total}
+	   */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  public void add (int value) {
+	  }
+
+	  public void subtract (int value) {
+	  }
+
+	  public String toString () {
+	    return "";
+	  }
+
+	  public void clear() {
+	  }
+	}
